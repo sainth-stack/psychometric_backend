3674bb8a53f24c232c3772613b5d2c98d1bdc0ed
sainth-stack <123sainathreddy100@gmail.com>
2025-01-25 09:59:23 +0530

    updated minor changes

--- config/db/ConnectDb.js
+++ config/db/ConnectDb.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+ const DATABASE_URL = `mongodb+srv://prashanth:BnHRQrqZHdnosfEe@cluster0.cpydc.mongodb.net/tax-filing5?retryWrites=true&w=majority`;
+
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(DATABASE_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- const/const.js
+++ const/const.js
@@ -1,6 +1,6 @@
 export const PORT = 4000;
 
 export const GOOGLE_CLIENT_ID =
-  "30649017116-ab4vo3o7ima0p5epe1otfn2a2dkoun0j.apps.googleusercontent.com";
+  "573823221354-d175srri1ta9un581atkp7b9qenst32u.apps.googleusercontent.com";
 
   export const JWT_SECRET="cdsbvj"
